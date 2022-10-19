832a583a2356d4e7be3fdbb13d413b04f9b8c685
Ruslan Salimov <work@salimov-dev.ru>
2022-10-19 16:01:33 +0300

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,4 @@
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
+import { getRandomColor } from './utils'
 
 export default function initApp() {
     console.log('Hello world');
